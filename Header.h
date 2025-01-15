5274ead1c84b9f7cfbf5f1384c17ab10db951a96
ushakovadiit14 <dielluvv707@gmail.com>
2025-01-15 09:50:31 +0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,14 @@
+#pragma once
+using namespace std;
+
+// функция для вычитания двух чисел
+string vichitanie(const string& a, const string& b);
+
+// функция для деления двух чисел
+string delenie(const string& a, const string& b);
+
+// функция, чтобы проверить на ошибки при вводи чисел
+bool oshibki(const string& input);
+
+// функция, чтобы убрать знак минус (если он вообще есть)
+string removeSign(const string& input);
